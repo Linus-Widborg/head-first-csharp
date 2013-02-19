38e4cb05b72836661b250cc86ef3e44557bbd8e5
Linus Widborg <linus.widborg@gmail.com>
2013-02-19 11:26:28 +0100

    The four button example
    Only partially completed

---
+++ linus/FourButtons/FourButtons/4Buttons.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace FourButtons
+{
+    partial class FourButtonsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PIbutton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PIbutton
+            // 
+            this.PIbutton.Location = new System.Drawing.Point(25, 22);
+            this.PIbutton.Name = "PIbutton";
+            this.PIbutton.Size = new System.Drawing.Size(75, 23);
+            this.PIbutton.TabIndex = 0;
+            this.PIbutton.Text = "PI";
+            this.PIbutton.UseVisualStyleBackColor = true;
+            this.PIbutton.Click += new System.EventHandler(this.PIbutton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(104, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(138, 157);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // FourButtonsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.PIbutton);
+            this.Name = "FourButtonsForm";
+            this.Text = "4Buttons";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PIbutton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ linus/FourButtons/FourButtons/4Buttons.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FourButtons
+{
+    public partial class FourButtonsForm : Form
+    {
+        public FourButtonsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PIbutton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("========================\n" + "The number of pi: " + Math.PI + "\n========================");
+        }
+    }
+}
