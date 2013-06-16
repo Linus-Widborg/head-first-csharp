f751b823fc2a0648b99c201140f13e9dd44526ce
macurban <macurban@auwa.se>
2013-06-16 10:04:40 +0200

    Skapade en ny fil för att testa

---
+++ macurban/Head-First-Csharp/Head-First-Csharp/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Head_First_Csharp
+{
+    class Class2
+    {
+        //Testar att skapa en fil
+    }
+}
